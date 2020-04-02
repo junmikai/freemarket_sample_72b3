0480a8f545ffad4af39672646135371dbaee6b57
nishikawasarina <sarisari.love@icloud.com>
2020-04-02 16:01:39 +0900

    controller model 作成

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,11 @@
+class DestinationsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
-  
+  resources :products, only: [:show, :new]
 end
