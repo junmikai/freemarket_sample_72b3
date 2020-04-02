cfc16515c45f1bf0aec8bcaecef71352b892ce83
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-02 20:29:56 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :category, dependent: :destroy
   has_many :images, dependent: :destroy
-  belongs_to_active_hash :prefecture
-  
+  # belongs_to_active_hash :prefecture
+
 end
