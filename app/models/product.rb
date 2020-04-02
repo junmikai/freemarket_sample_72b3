a7629217c20896f8185c49b73966ba091fa704fd
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-02 20:40:42 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :user, dependent: :destroy
   belongs_to :category, dependent: :destroy
   has_many :images, dependent: :destroy
-  belongs_to_active_hash :prefecture
+  # belongs_to_active_hash :prefecture
 
 end
