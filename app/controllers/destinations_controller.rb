988a13860da22d1cafa5f5c0e424c1d4284961b9
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-03 10:51:45 +0900

    発送元・お届け先住所入力のファイル名変更

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,6 +3,10 @@ class DestinationsController < ApplicationController
   def new
   end
 
+
+  def show
+  end
+
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
   resources :products, only: [:show, :new]
-  resources :destinations, only: [:new]
+  resources :destinations, only: [:new, :show]
   resources :users, only: [:index]
 end
