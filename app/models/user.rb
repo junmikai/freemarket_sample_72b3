7c676a697987e292968f76e0f37e7152acf84332
taihei555 <ipadpro11mex@gmail.com>
2020-04-03 16:46:19 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable,
+      :validatable, password_length: 7..20
+  # validates :password_length, length: {minimum: 7}
+  
+  validates :email, uniqueness: true
+  validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  
+  
+        #  validates :family_name, format: { with: /^[ぁ-んァ-ヶー一-龠]+$/ }
+
 
   
 # 適用したらログイン機能が反映されないため一旦外してます
