bdf52e96d72fc91bda954d1782f38af8bbcd5e9c
nishikawasarina <sarisari.love@icloud.com>
2020-04-07 10:43:50 +0900

    画像複数投稿

--- db/migrate/20200402064035_create_images.rb
+++ db/migrate/20200402064035_create_images.rb
@@ -1,10 +1,10 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :image1, null:false
-      t.string :image2
-      t.string :image3
-      t.string :image4
+      t.string :image
+      # t.string :image2
+      # t.string :image3
+      # t.string :image4
       t.integer :product_id, null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_064105) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image1", null: false
-    t.string "image2"
-    t.string "image3"
-    t.string "image4"
+    t.string "image"
     t.integer "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
