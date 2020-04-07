76de539351238a2ab3504ac0d7d2b1d0727e1bce
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-07 19:27:58 +0900

    js前

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   
+  
   def show
   end
 
@@ -28,6 +29,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :price, :description, :status, :shipping_cost, :shipping_days, :category_id, :prefecture_id, :distination_id, images_attributes: {image: []}).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :price, :description, :status, :shipping_cost, :shipping_days, :category_id, :prefecture_id, :distination_id, images_attributes:[:image]).merge(user_id: current_user.id)
   end
 end
