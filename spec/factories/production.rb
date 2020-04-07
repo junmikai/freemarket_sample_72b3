f7abae79042ea91e4ff405f233136dcf831a391c
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-07 18:28:04 +0900

    コメント

--- spec/factories/production.rb
+++ spec/factories/production.rb
@@ -1,15 +1,16 @@
-FactoryBot.define do
+# FactoryBot.define do
 
-  factory :production do
-    name               {"abe"}
-    price              {"kkk@gmail.com"}
-    status             {"00000000"}
-    shipping_cost      {"00000000"}
-    shipping_days      {"00000000"}
-    prefecture_id      {"00000000"}
-    category_id        {"00000000"}
-    destination_id     {"00000000"}
-    user_id            {"00000000"}
-  end
+#   factory :product do
+#     name               {"abe"}
+#     description        {"やすい"}
+#     price              {"1000"}
+#     status             {"00000000"}
+#     shipping_cost      {"00000000"}
+#     shipping_days      {"00000000"}
+#     prefecture_id      {"00000000"}
+#     category_id        {"00000000"}
+#     # destination_id     {"00000000"}
+#     user_id            {"00000000"}
+#   end
 
-end
+# end
