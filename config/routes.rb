90a255c6bebdf29695b3410a37120acbfca6e17d
kinba0326 <kinba.tsukasa@icloud.com>
2020-04-09 08:49:22 +0900

    カテゴリー1

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
   resources :products, only: [:show, :new, :create]
+
   resources :products do
-    resources :images, only: [:create]
+    resources :images, only: [:create, ]
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get<div class="code-title" data-title="Gemfile">_category_grandchildren', defaults: { format: 'json' }
+    end
   end
   resources :destinations, only: [:new, :show]
   resources :users, only: [:show]
