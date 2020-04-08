cedc13aaaca158a8ea92d20df428957c0cc0cdff
nishikawasarina <sarisari.love@icloud.com>
2020-04-08 14:05:22 +0900

    画像表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,10 @@ class TopsController < ApplicationController
 
   def index
     @products = Product.all
+  #   if params[:image] != nil
+  #     image = MiniMagick::Image.read(params[:image])
+  #     image.resize "300x300"
+  #     image.write "public/images/hoge.jpg"
+  #  end
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   include CarrierWave::MiniMagick
-
+  process resize_to_fill: [220, 160, "center"]
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [50, 50]
+  # process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
