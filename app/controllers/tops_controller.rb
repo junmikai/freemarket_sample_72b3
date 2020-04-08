498f2381f9d0a2dfeaba66a28046cea04238ab3d
nishikawasarina <sarisari.love@icloud.com>
2020-04-08 15:42:16 +0900

    コメントアウト　削除

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,10 +2,5 @@ class TopsController < ApplicationController
 
   def index
     @products = Product.all
-  #   if params[:image] != nil
-  #     image = MiniMagick::Image.read(params[:image])
-  #     image.resize "300x300"
-  #     image.write "public/images/hoge.jpg"
-  #  end
   end
 end
