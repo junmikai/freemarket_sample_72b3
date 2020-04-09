677939022943076fb6aed115d91e7e54508abc97
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-09 12:00:06 +0900

    コメントアウトなくし

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   process resize_to_fill: [220, 160, "center"]
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
