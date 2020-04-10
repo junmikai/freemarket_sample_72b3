80a6d16c08dda97c3411369fcdcca3d0ea7afcca
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-10 18:35:37 +0900

    クレジットカード新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
   end
   resources :destinations, only: [:new, :show]
   resources :users, only: [:show]
+
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ユーザーおよびカードの登録
+      get 'done' # 登録完了後のページ
+    end
+  end
 end
