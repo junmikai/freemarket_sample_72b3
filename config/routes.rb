bb0f75eff85bca2b1eb758ade2a1167b39e07991
taihei555 <ipadpro11mex@gmail.com>
2020-04-13 10:47:20 +0900

    Merge branch '買い手情報' of https://github.com/junmikai/freemarket_sample_72b3 into 買い手情報

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   root 'tops#index'
   resources :products, only: [:show, :new, :create] do
     collection do
+
       get 'buy'
+
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+
     end
   end
   resources :products do
-    resources :images, only: [:create]
+    resources :images, only: [:create, ]
   end
-  resources :destinations, only: [:new, :create, :show]
+  resources :destinations, only: [:new, :create, :show, :edit, :update]
   resources :users, only: [:show]
 end
