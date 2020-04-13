b3beef16239c560b96716a285e55d34880121e44
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-04-13 11:44:07 +0900

    Update product.js

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -29,19 +29,3 @@ $(document).on('turbolinks:load', ()=> {
 });
 
 
-
-
-
-
-
-$(function(){
-  $('.commentBtn').on('click', function(){
-    $(".productcheck").show()
- });
-})
-
-$(function(){
-  $('.productcheck').on('mouseleave', function(){
-    $(this).hide()
- });
-})
